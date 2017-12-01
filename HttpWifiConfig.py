3691a0b995308abf0554b66238075e96756c41fd
SaraQian <abigail830@163.com>
2017-12-01 15:38:09 +0000

    Using 2 diff route path

--- HttpWifiConfig.py
+++ HttpWifiConfig.py
@@ -4,9 +4,15 @@ import WifiConfig
  
 app = picoweb.WebApp(__name__)
  
-@app.route("/")
-def index(req, resp):
+@app.route("/WifiConfig")
+def getwificonfig(req, resp):
    wifiProfileJson = ujson.dumps(WifiConfig.getWifiProfile())
    yield from picoweb.start_response(resp, content_type = "application/json")
    yield from resp.awrite(wifiProfileJson)
+
+@app.route("/")
+def index(req,resp):
+   yield from picoweb.start_response(resp,content_type = "text/plain")
+   yield from resp.awrite("Hello World!")
+
 
