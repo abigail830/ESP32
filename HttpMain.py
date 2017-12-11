331c4a766ad3b0402a4a334fc68e226334fe2c26
Sara <abigail830@163.com>
2017-12-11 22:46:48 +0800

    HttpPost handle

--- HttpMain.py
+++ HttpMain.py
@@ -7,12 +7,9 @@ app = picoweb.WebApp(__name__)
 @app.route("/wifi")
 def getwificonfig(req, resp):
    if(req.method == 'GET'):
-      wifiProfileJson = ujson.dumps(WifiConfig.getWifiProfile())
-      yield from picoweb.start_response(resp, content_type = "application/json")
-      yield from resp.awrite(wifiProfileJson)
+      picoweb.jsonify(resp, WifiConfig.getWifiProfile())
    else:
-      postData = req.read_form_data()
-      print(postData)
+      print(req.form)
       yield from picoweb.start_response(resp, content_type="text/html")
       yield from resp.awrite("Http Post received.")
 
