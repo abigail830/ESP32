cbc014f500edb5335ef6336a622306421a979348
Sara <abigail830@163.com>
2017-12-11 22:15:35 +0800

    adding http post handle

--- HttpMain.py
+++ HttpMain.py
@@ -6,9 +6,13 @@ app = picoweb.WebApp(__name__)
  
 @app.route("/wifi")
 def getwificonfig(req, resp):
-   wifiProfileJson = ujson.dumps(WifiConfig.getWifiProfile())
-   yield from picoweb.start_response(resp, content_type = "application/json")
-   yield from resp.awrite(wifiProfileJson)
+   if(req.method == 'GET'):
+      wifiProfileJson = ujson.dumps(WifiConfig.getWifiProfile())
+      yield from picoweb.start_response(resp, content_type = "application/json")
+      yield from resp.awrite(wifiProfileJson)
+   else:
+      postData = ujson.loads(req.data)
+      print(postData)
 
 @app.route("/")
 def index(req,resp):
